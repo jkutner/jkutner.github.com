74cadbe7f9f2480b5ce453b830a421f4e77879c0
Joe Kutner <jpkutner@gmail.com>
2015-02-24 09:32:48 -0600

    prettier publish script

--- publish.sh
+++ publish.sh
@@ -3,10 +3,12 @@
 echo "--> Building site..."
 jekyll build
 
-echo "\n--> Updating RSS Feed..."
+echo ""
+echo "--> Updating RSS Feed..."
 cp _site/rss.xml rss.xml
 git add rss.xml
 git commit -m "Updating RSS Feed"
 
-echo "\n--> Publishing site..."
+echo ""
+echo "--> Publishing site..."
 git push origin $(git name-rev HEAD 2> /dev/null | sed 's#HEAD\ \(.*\)#\1#'):master
