8c343f05ddeb0daa65bb4a7c68cb077c38a3507f
Joe Kutner <jpkutner@gmail.com>
2015-02-24 09:31:24 -0600

    Added publishing script to include rss feed

---
+++ publish.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "Building site..."
+jekyll build
+
+echo "Updating RSS Feed..."
+cp _site/rss.xml rss.xml
+git add rss.xml
+git commit -m "Updating RSS Feed"
+
+echo "Publishing site..."
+git push origin $(git name-rev HEAD 2> /dev/null | sed 's#HEAD\ \(.*\)#\1#'):master
