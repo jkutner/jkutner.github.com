5888a4c04c69804bcb941d1a0856dd812bfb7a49
Joe Kutner <jpkutner@gmail.com>
2015-05-24 11:35:43 -0500

    Updating config

--- publish.sh
+++ publish.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-echo "--> Building site..."
-jekyll build
-
-echo ""
-echo "--> Updating RSS Feed..."
-cp _site/rss.xml rss.xml
-git add rss.xml
-git commit -m "Updating RSS Feed"
-
 echo ""
 echo "--> Publishing site..."
 git push origin $(git name-rev HEAD 2> /dev/null | sed 's#HEAD\ \(.*\)#\1#'):master
